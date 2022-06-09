0f8cf2ec41d510a968f32abc06e9d06e0ffc903f
Rwema <52289151+rwema3@users.noreply.github.com>
2022-06-09 15:33:06 +0200

    Update highlight_focus.dart

--- lib/layout/highlight_focus.dart
+++ lib/layout/highlight_focus.dart
@@ -1,5 +1,7 @@
 // Copyright 2019 The Flutter team. All rights reserved.
 // Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
 
 
   /// [child] is your widget.
