c285a91b79a02383518a0627b0e456f864d58fd7
Rwema <52289151+rwema3@users.noreply.github.com>
2022-07-15 10:49:29 +0200

    if (path.openInSecondScreen) {           return TwoPanePageRoute<void>(             builder: (context) => path.builder(context, match),             settings: settings,           );         } else {           return MaterialPageRoute<void>(             builder: (context) => path.builder(context, match),             settings: settings,           );         }       }     }

--- lib/routes.dart
+++ lib/routes.dart
@@ -91,6 +91,20 @@ class RouteConfiguration {
             // ignore: prefer_const_constructors
             () => fortnightly.FortnightlyApp()),
       ),
+        if (path.openInSecondScreen) {
+          return TwoPanePageRoute<void>(
+            builder: (context) => path.builder(context, match),
+            settings: settings,
+          );
+        } else {
+          return MaterialPageRoute<void>(
+            builder: (context) => path.builder(context, match),
+            settings: settings,
+          );
+        }
+      }
+    }
+
     // If no match was found, we let [WidgetsApp.onUnknownRoute] handle it.
     return null;
   }
