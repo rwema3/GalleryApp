6690263c7a55dceb63e30a8d96570e908ab2ad44
Rwema <52289151+rwema3@users.noreply.github.com>
2022-07-15 14:18:41 +0200

    Update routes.dart

--- lib/routes.dart
+++ lib/routes.dart
@@ -91,6 +91,7 @@ class RouteConfiguration {
             // ignore: prefer_const_constructors
             () => fortnightly.FortnightlyApp()),
       ),
+          return NoAnimationMaterialPageRoute<void>(
             builder: (context) => path.builder(context, match),
             settings: settings,
           );
