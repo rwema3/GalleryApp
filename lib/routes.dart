21d896577737a6239d2a50072c2e90e4b5c78453
Rwema <52289151+rwema3@users.noreply.github.com>
2022-07-15 10:48:54 +0200

    Update routes.dart

--- lib/routes.dart
+++ lib/routes.dart
@@ -91,24 +91,6 @@ class RouteConfiguration {
             // ignore: prefer_const_constructors
             () => fortnightly.FortnightlyApp()),
       ),
-  
-
-  /// The route generator callback used when the app is navigated to a named
-  /// route. Set it on the [MaterialApp.onGenerateRoute] or
-  /// [WidgetsApp.onGenerateRoute] to make use of the [paths] for route
-  /// matching.
-  static Route<dynamic>? onGenerateRoute(RouteSettings settings) {
-    for (final path in paths) {
-      final regExpPattern = RegExp(path.pattern);
-      if (regExpPattern.hasMatch(settings.name!)) {
-        final firstMatch = regExpPattern.firstMatch(settings.name!)!;
-        final match = (firstMatch.groupCount == 1) ? firstMatch.group(1) : null;
-        if (kIsWeb) {
-          return NoAnimationMaterialPageRoute<void>(
-            builder: (context) => path.builder(context, match),
-            settings: settings,
-          );
-        }
         if (path.openInSecondScreen) {
           return TwoPanePageRoute<void>(
             builder: (context) => path.builder(context, match),
