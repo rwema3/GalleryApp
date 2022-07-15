2a980a5b48a73924f3fe78579f94f148b2b5d6ae
Rwema <52289151+rwema3@users.noreply.github.com>
2022-07-15 13:12:30 +0200

    Update routes.dart

--- lib/routes.dart
+++ lib/routes.dart
@@ -83,7 +83,20 @@ class RouteConfiguration {
       ),
       openInSecondScreen: true,
     ),
-  
+        if (path.openInSecondScreen) {
+          return TwoPanePageRoute<void>(
+            builder: (context) => path.builder(context, match),
+            settings: settings,
+          );
+        } else {
+          return MaterialPageRoute<void>(
+            builder: (context) => path.builder(context, match),
+            settings: settings,
+          );
+        }
+      }
+    }
+
     // If no match was found, we let [WidgetsApp.onUnknownRoute] handle it.
     return null;
   }
