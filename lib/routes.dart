b727085aee48833e374303bb09232ed094522dff
Rwema <52289151+rwema3@users.noreply.github.com>
2022-07-15 14:18:51 +0200

    Update routes.dart

--- lib/routes.dart
+++ lib/routes.dart
@@ -91,6 +91,7 @@ class RouteConfiguration {
             // ignore: prefer_const_constructors
             () => fortnightly.FortnightlyApp()),
       ),
+      if (regExpPattern.hasMatch(settings.name!)) {
         final firstMatch = regExpPattern.firstMatch(settings.name!)!;
         final match = (firstMatch.groupCount == 1) ? firstMatch.group(1) : null;
         if (kIsWeb) {
