45b84165bdc0bda7dc4df6c8d112b0212a49c42f
Rwema <52289151+rwema3@users.noreply.github.com>
2022-07-15 14:17:35 +0200

    Update routes.dart

--- lib/routes.dart
+++ lib/routes.dart
@@ -91,7 +91,6 @@ class RouteConfiguration {
             // ignore: prefer_const_constructors
             () => fortnightly.FortnightlyApp()),
       ),
-          return MaterialPageRoute<void>(
             builder: (context) => path.builder(context, match),
             settings: settings,
           );
