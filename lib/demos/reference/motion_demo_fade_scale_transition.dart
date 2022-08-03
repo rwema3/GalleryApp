4aba58de27a91e1e13c60d14068cd8aa0927f892
Dominique Rwema Bagirishya <52289151+rwema3@users.noreply.github.com>
2022-08-04 00:06:12 +0200

    Update motion_demo_fade_scale_transition.dart

--- lib/demos/reference/motion_demo_fade_scale_transition.dart
+++ lib/demos/reference/motion_demo_fade_scale_transition.dart
@@ -20,6 +20,69 @@ class FadeScaleTransitionDemo extends StatefulWidget {
     super.dispose();
   }
 
+  @override
+  Widget build(BuildContext context) {
+    final localizations = GalleryLocalizations.of(context)!;
+
+    return Scaffold(
+      appBar: AppBar(
+        automaticallyImplyLeading: false,
+        title: Column(
+          children: [
+            Text(localizations.demoFadeScaleTitle),
+            Text(
+              '(${localizations.demoFadeScaleDemoInstructions})',
+              style: Theme.of(context)
+                  .textTheme
+                  .subtitle2!
+                  .copyWith(color: Colors.white),
+            ),
+          ],
+        ),
+      ),
+      floatingActionButton: AnimatedBuilder(
+        animation: _controller,
+        builder: (context, child) {
+          return FadeScaleTransition(
+            animation: _controller,
+            child: child,
+          );
+        },
+        child: Visibility(
+          visible: _controller.status != AnimationStatus.dismissed,
+          child: FloatingActionButton(
+            onPressed: () {},
+            child: const Icon(Icons.add),
+          ),
+        ),
+      ),
+      bottomNavigationBar: Column(
+        mainAxisSize: MainAxisSize.min,
+        children: [
+          const Divider(height: 0),
+          Padding(
+            padding: const EdgeInsets.symmetric(vertical: 8),
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                ElevatedButton(
+                  onPressed: () {
+                    showModal<void>(
+                        context: context,
+                        builder: (context) => _showExampleAlertDialog());
+                  },
+                  child: Text(localizations.demoFadeScaleShowAlertDialogButton),
+                ),
+                const SizedBox(width: 10),
+                ElevatedButton(
+                  onPressed: () {
+                    if (_isAnimationRunningForwardsOrComplete) {
+                      _controller.reverse();
+                    } else {
+                      _controller.forward();
+                    }
+                  },
+                  child: Text(
                     _isAnimationRunningForwardsOrComplete
                         ? localizations.demoFadeScaleHideFabButton
                         : localizations.demoFadeScaleShowFabButton,
