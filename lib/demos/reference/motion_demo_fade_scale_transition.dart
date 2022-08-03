69a6994534373c88ca1d87df005fcaa02d13744f
Dominique Rwema Bagirishya <52289151+rwema3@users.noreply.github.com>
2022-08-04 00:05:43 +0200

    Update motion_demo_fade_scale_transition.dart

--- lib/demos/reference/motion_demo_fade_scale_transition.dart
+++ lib/demos/reference/motion_demo_fade_scale_transition.dart
@@ -1,25 +1,3 @@
-// Copyright 2019 The Flutter Authors. All rights reserved.
-// Use of this source code is governed by a BSD-style license that can be
-// found in the LICENSE file.
-
-import 'package:animations/animations.dart';
-import 'package:flutter/material.dart';
-import 'package:flutter_gen/gen_l10n/gallery_localizations.dart';
-
-// BEGIN fadeScaleTransitionDemo
-
-class FadeScaleTransitionDemo extends StatefulWidget {
-  const FadeScaleTransitionDemo({super.key});
-
-  @override
-     _FadeScaleTransitionDemoState();
-}
-  @override
-  void dispose() {
-    _controller.dispose();
-    super.dispose();
-  }
-
             Navigator.of(context).pop();
           },
           child: Text(localizations.demoFadeScaleAlertDialogDiscardButton),
