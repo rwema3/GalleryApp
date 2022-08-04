506e367ed14b24762401cd623f905970e30be6ae
Dominique Rwema Bagirishya <52289151+rwema3@users.noreply.github.com>
2022-08-05 00:09:03 +0200

    Update motion_demo_fade_scale_transition.dart

--- lib/demos/reference/motion_demo_fade_scale_transition.dart
+++ lib/demos/reference/motion_demo_fade_scale_transition.dart
@@ -5,6 +5,23 @@
 import 'package:animations/animations.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_gen/gen_l10n/gallery_localizations.dart';
+      bottomNavigationBar: Column(
+        mainAxisSize: MainAxisSize.min,
+        children: [
+          const Divider(height: 0),
+          Padding(
+            padding: const EdgeInsets.symmetric(vertical: 8),
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                ElevatedButton(
+                  onPressed: () {
+                    showModal<void>(
+                        context: context,
+                        builder: (context) => _showExampleAlertDialog());
+                  },
+                  child: Text(localizations.demoFadeScaleShowAlertDialogButton),
+                ),
                 const SizedBox(width: 10),
                 ElevatedButton(
                   onPressed: () {
