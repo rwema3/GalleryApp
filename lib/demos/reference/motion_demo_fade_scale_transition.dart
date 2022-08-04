86f10b269e7d92631c7f82e3e42647c244bafe59
Dominique Rwema Bagirishya <52289151+rwema3@users.noreply.github.com>
2022-08-05 00:08:58 +0200

    Update motion_demo_fade_scale_transition.dart

--- lib/demos/reference/motion_demo_fade_scale_transition.dart
+++ lib/demos/reference/motion_demo_fade_scale_transition.dart
@@ -1,10 +1,4 @@
-// Copyright 2019 The Flutter Authors. All rights reserved.
-// Use of this source code is governed by a BSD-style license that can be
-// found in the LICENSE file.
 
-import 'package:animations/animations.dart';
-import 'package:flutter/material.dart';
-import 'package:flutter_gen/gen_l10n/gallery_localizations.dart';
                 const SizedBox(width: 10),
                 ElevatedButton(
                   onPressed: () {
