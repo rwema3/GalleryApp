799e427f9368421d5441d2d89cf9b4d9211a9c22
Dominique Rwema Bagirishya <52289151+rwema3@users.noreply.github.com>
2022-08-06 19:20:11 +0200

    Update motion_demo_fade_scale_transition.dart

--- lib/demos/reference/motion_demo_fade_scale_transition.dart
+++ lib/demos/reference/motion_demo_fade_scale_transition.dart
@@ -9,14 +9,7 @@ import 'package:flutter_gen/gen_l10n/gallery_localizations.dart';
 // BEGIN fadeScaleTransitionDemo
 
 class FadeScaleTransitionDemo extends StatefulWidget {
-  const FadeScaleTransitionDemo({super.key});
-
-  @override
-  State<FadeScaleTransitionDemo> createState() =>
-      _FadeScaleTransitionDemoState();
-}
-
-class _FadeScaleTransi
+  
   @override
   void initState() {
     super.initState();
