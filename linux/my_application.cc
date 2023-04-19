72d22beb5628208b29a9b80b877ec4c9ec3416b9
rwemadominik3@gmail.com <52289151+rwema3@users.noreply.github.com>
2023-04-19 23:50:29 +0200

    my_application_new

--- linux/my_application.cc
+++ linux/my_application.cc
@@ -96,3 +96,10 @@ static void my_application_class_init(MyApplicationClass* klass) {
 }
 
 static void my_application_init(MyApplication* self) {}
+
+MyApplication* my_application_new() {
+  return MY_APPLICATION(g_object_new(my_application_get_type(),
+                                     "application-id", APPLICATION_ID,
+                                     "flags", G_APPLICATION_NON_UNIQUE,
+                                     nullptr));
+}
