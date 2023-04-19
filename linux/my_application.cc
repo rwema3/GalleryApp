7dd8719b9db9278f311b2d07e1ea9f2c902b7894
rwemadominik3@gmail.com <52289151+rwema3@users.noreply.github.com>
2023-04-19 23:49:10 +0200

    endif

--- linux/my_application.cc
+++ linux/my_application.cc
@@ -35,4 +35,17 @@ static void my_application_activate(GApplication* application) {
      if (g_strcmp0(wm_name, "GNOME Shell") != 0) {
        use_header_bar = FALSE;
      }
+  }
+#endif
+  if (use_header_bar) {
+    GtkHeaderBar *header_bar = GTK_HEADER_BAR(gtk_header_bar_new());
+    gtk_widget_show(GTK_WIDGET(header_bar));
+    gtk_header_bar_set_title(header_bar, "Flutter Gallery");
+    gtk_header_bar_set_show_close_button(header_bar, TRUE);
+    gtk_window_set_titlebar(window, GTK_WIDGET(header_bar));
+  }
+  else {
+    gtk_window_set_title(window, "Flutter Gallery");
+  }
+
 
